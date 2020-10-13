4fed57380290172fbd3376dc1ce9a6cad5c864e4
rahul <rahul24rokade@gmail.com>
2020-10-13 14:38:38 +0530

    Updates

---
+++ precache-manifest.bcae88eb7997987983906b062ceee676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4246b72bd90b1a3d9c6387feccb40488",
+    "url": "/react_project/index.html"
+  },
+  {
+    "revision": "d5ec6ccc5ee1e92e3161",
+    "url": "/react_project/static/css/main.11c4e233.chunk.css"
+  },
+  {
+    "revision": "8efd4284e34443de5126",
+    "url": "/react_project/static/js/2.466a5494.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_project/static/js/2.466a5494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ec6ccc5ee1e92e3161",
+    "url": "/react_project/static/js/main.bbcb992b.chunk.js"
+  },
+  {
+    "revision": "2e488b6aaca91fd22237",
+    "url": "/react_project/static/js/runtime-main.20855e57.js"
+  },
+  {
+    "revision": "bd72eb93f3238add18697d62d1bc9deb",
+    "url": "/react_project/static/media/facebook-reactions.bd72eb93.png"
+  }
+]);
